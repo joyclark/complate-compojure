93a5d226bd64d4346049c689b7a0aa1198764d18
Joy Clark <joy.clark@innoq.com>
2017-05-29 22:06:56 +0200

    First commit for complate compojure

---
+++ src/complate_compojure/core.clj
@@ -0,0 +1,26 @@
+(ns complate-compojure.core
+  (:import [complate ScriptingBridge]
+           [java.io OutputStream])
+  (:require [ring.core.protocols :refer [StreamableResponseBody]]
+            [ring.adapter.jetty :as jetty]
+            [complate-compojure.handler :refer [app]]))
+
+(def engine (new ScriptingBridge))
+
+(defprotocol Renderer 
+  (render [this stream]))
+
+(defrecord ComplateRenderer [file-name fn-name args]
+  Renderer
+  (render [this stream]
+    (.invoke engine file-name fn-name stream (into-array stream))))
+
+(defn response [file-name fn-name args]
+  (->ComplateRenderer file-name fn-name args))
+
+(extend-type ring.core.protocols.StreamableResponseBody
+  Renderer
+  (write-body-to-stream [renderer response output-stream]))
+
+(defn -main []
+  (jetty/run-jetty app {:port 3030}))
